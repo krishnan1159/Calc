9dee6ed21ad2166dda37b6ca1450df7f0cdd8d4e
unknown <aariyankrishnan@gmail.com>
2013-01-15 16:30:06 +0530

    First commit

---
+++ test1.php
@@ -0,0 +1,12 @@
+<table border='1' cellspacing=0 >
+	<tr>
+		<td><input type="text" /></td>
+		<td><input type="text" /></td>
+		<td><input type="text" /></td>
+	</tr>
+	<tr>
+		<td>ft cell</td>
+		<td>sd cell</td>
+		<td>Td Cell</td>
+	</tr>
+</table>
